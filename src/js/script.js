6525d52f28378674bf7fded4ed0c0ccffa9da0e3
Lucas-Ielsch <lucas.ielsch@edu.univ-fcomte.fr>
2024-11-08 14:19:55 +0100

    Fin du TP

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,26 @@
 // TODO
+
+//changer la couleur du texte 
+
+const aMettreEnRouge = document.querySelector("#a-mettre-en-rouge")
+
+aMettreEnRouge.style.color = "red"
+
+//Réagir a un click 
+
+const EnRougeSuiteAClick = document.querySelector("#en-rouge-suite-a-click")
+
+EnRougeSuiteAClick.addEventListener("click", (evt) =>{
+    EnRougeSuiteAClick.style.color = "red" });
+
+//Collections D'éléments 
+
+collH2 = document.querySelectorAll ("h2")
+
+NodeList(3) [h2, h2, h2]
+
+collH2.forEach((elm)=>{ elm.addEventListener("click",(evt) =>{ evt.target.style.color = "red" ; 
+    }); 
+});
+
+//Finis 
